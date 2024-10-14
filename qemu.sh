649ea2f3a5f3771dc5819763fcc73f72b65b664a
rob-scheepens <rob.scheepens@nutanix.com>
2024-10-14 12:54:35 +0200

    Correcting script.

--- qemu.sh
+++ qemu.sh
@@ -29,7 +29,7 @@ set -x
 	-m 150G \
 	-drive file=$GUEST_IMAGE,if=none,id=nvme0,format=qcow2,snapshot=on \
 	-device nvme,drive=nvme0,serial=1234 \
-	-netdev tap,id=host,ifname=vm-nic,script=no,downscript=no \  #,hostfwd=tcp::5900-:5900,hostfwd=tcp::$SSH_PORT-:$SSH_PORT,hostfwd=tcp::3389-:3389,hostfwd=tcp::$GUEST_CONSOLE_PORT-:$GUEST_CONSOLE_PORT,hostfwd=tcp::$HOST_TELNET_PORT-:$HOST_TELNET_PORT,hostfwd=tcp::$GDB_PORT-:$GDB_PORT \
+	-netdev tap,id=host,ifname=vm-nic,script=no,downscript=no \  
 	-device e1000,netdev=host \
 	-display none \
 	-device virtio-serial-pci \
@@ -41,3 +41,7 @@ set -x
 	-append "console=hvc0 kvm_intel.dump_invalid_vmcs=1 nokaslr vfio_iommu_type1.allow_unsafe_interrupts=1 $1 $2" \
 	-virtfs local,path=$SHARE_DIR,mount_tag=host,security_model=none \
 	-vnc :1
+	
+	#,hostfwd=tcp::5900-:5900,hostfwd=tcp::$SSH_PORT-:$SSH_PORT,hostfwd=tcp::3389-:3389,hostfwd=tcp::$GUEST_CONSOLE_PORT-:$GUEST_CONSOLE_PORT,hostfwd=tcp::$HOST_TELNET_PORT-:$HOST_TELNET_PORT,hostfwd=tcp::$GDB_PORT-:$GDB_PORT \
+
+	

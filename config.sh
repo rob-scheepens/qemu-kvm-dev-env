6a236ab7ff11f5151905ac4c1a3c2f03025e0c31
Rob Scheepens <rob.scheepens@nutanix.com>
2025-08-12 04:03:30 -0400

    Changing config and inittab to use run_br0.sh when starting.
    Signed-off-by: Rob Scheepens <rob.scheepens@nutanix.com>

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@ export KERNEL_BUILD_DIR="$ROOT_DIR/linux"
 export KVM_UNIT_TESTS_BUILD_DIR="$ROOT_DIR/kvm-unit-tests"
 
 export WORK_DIR="$ROOT_DIR/qemu-kvm-dev-env"
-export GUEST_IMAGE="/home/nutanix/rob-win2019-vsm-uefi.qcow2"
+export GUEST_IMAGE="/home/nutanix/win2025.qcow2"
 
 # No need to change the defines below, the defaults should be mostly fine
 export SHARE_DIR="$WORK_DIR/share"
@@ -20,5 +20,5 @@ export HOST_TELNET_PORT="1237"
 export GUEST_CONSOLE_PORT="1235"
 export GDB_PORT="1234"
 
-export CPUS="16"
-export MEM="64G"
+export CPUS="4"
+export MEM="32G"
